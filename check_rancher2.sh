d93c4dee2b0b8a595538c128318535e4c9e27a0a
Napsty <ck@claudiokuenzler.com>
2021-05-04 11:11:24 +0200

    Set release date for 1.6.0

--- check_rancher2.sh
+++ check_rancher2.sh
@@ -42,7 +42,7 @@
 # 20200617 1.3.0 Added ignore parameter (-i)                                             #
 # 20210210 1.4.0 Checking specific workloads and pods inside a namespace                 #
 # 20210413 1.5.0 Plugin now uses jq instead of jshon, fix cluster error check (#19)      #
-# 20210416 1.6.0 Add usage performance data on single cluster check, fix project check   #
+# 20210504 1.6.0 Add usage performance data on single cluster check, fix project check   #
 ##########################################################################################
 # (Pre-)Define some fixed variables
 STATE_OK=0              # define the exit code if status is OK
